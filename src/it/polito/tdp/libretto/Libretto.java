8703c3e9b159e8631477fc5ee3f05e4fe1c1af97
combi <combi@DESKTOP-8KHIN8V>
2019-03-18 13:56:40 +0100

    Punto 2

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -18,4 +18,18 @@ public class Libretto {
 		voti.add(v);
 	}
 	
+	/**
+	 * Seleziona il sottoinsieme di voti che hanno il punteggio specificato
+	 * @param punti punteggio da ricerca
+	 * @return lista di {@link Voto} aventi quel punteggio
+	 */
+	public List<Voto> cercaVoti(int punti) {
+		List<Voto> result = new ArrayList<Voto>();
+		for(Voto v : this.voti) {
+			if(v.getPunti()==punti) {
+				result.add(v);
+			}
+		}
+		return result;
+	}
 }
--- src/it/polito/tdp/libretto/TestLibretto.java
+++ src/it/polito/tdp/libretto/TestLibretto.java
@@ -1,6 +1,7 @@
 package it.polito.tdp.libretto;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class TestLibretto {
 
@@ -11,8 +12,11 @@ public class TestLibretto {
 		libr.add(new Voto(25, "Analisi 2", LocalDate.of(2018, 1, 15)));
 		libr.add(new Voto(28, "Fisica 1", LocalDate.of(2017, 2, 15)));
 		libr.add(new Voto(25, "Geometria", LocalDate.of(2017, 2, 18)));
-		libr.add(new Voto(18, "Chimica", LocalDate.of(2018, 1, 19)));
+		libr.add(new Voto(25, "Chimica", LocalDate.of(2018, 1, 19)));
 		libr.add(new Voto(19, "Programmmazione a oggetti", LocalDate.of(2018, 2, 7)));
+		
+		List<Voto> venticinque = libr.cercaVoti(25);
+		System.out.println(venticinque);
 	}
 
 }
